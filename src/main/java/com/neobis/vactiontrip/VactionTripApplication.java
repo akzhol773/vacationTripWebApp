b231dcc1db7bb81309dcdbc7d655492ae3192f9a
akzholbek <akzholbek0096@gmail.com>
2024-02-14 14:04:54 +0000

    careated empty packages

---
+++ src/main/java/com/neobis/vactiontrip/VactionTripApplication.java
@@ -0,0 +1,13 @@
+package com.neobis.vactiontrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VactionTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VactionTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neobis/vactiontrip/VactionTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.vactiontrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VactionTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
